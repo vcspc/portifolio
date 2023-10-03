235c767e5cd196e1f433652d4edfbf180433d094
Vinícius Costa <vitonilcius@gmail.com>
2023-10-03 19:12:51 -0300

    adicionado transições em todas as paginas

---
+++ js/transicao-direita.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    var elemento = document.querySelector('.transicao-direita');
+
+    elemento.classList.add('ativo-direita');
+    
+});
---
+++ js/transicao-esquerda.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    var elemento = document.querySelector('.transicao-esquerda');
+
+    elemento.classList.add('ativo-esquerda');
+    
+});
